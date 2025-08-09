0188fa231e94fe867c10a17775d72d9a5755abd4
yeskarthikeyanshanmugam <yeskarthikeyanshanmugam@gmail.com>
2025-08-09 10:40:52 +0530

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="read-the-docs">
+        {count}
+      </p>
+    </>
+  )
+}
+
+export default App
